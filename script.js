832a9d6627e781e4bdd8d509c8965aa0c9675af7
brendanbloom <136516241+brendanbloom@users.noreply.github.com>
2023-06-13 17:56:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 const apiUrl = 'https://brendanbloom.github.io/backendanalyst/';  // Replace with the URL of your backend API
 
-// Make a GET request to the backend API
 fetch(apiUrl)
   .then(response => response.json())
   .then(data => {
-    // Process the response data
-    console.log(data);
+    const dataContainer = document.getElementById('data-container');
+    dataContainer.textContent = data.message;
   })
   .catch(error => {
-    // Handle any errors
     console.error('Error:', error);
   });
